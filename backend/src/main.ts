b35c6b3a839ca4000385cde762657a9e4e79f2e0
後藤颯斗 <gotohayato@gotousatsutonoMacBook-Pro.local>
2023-02-12 17:49:24 +0900

    ちょっとだけ修正

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder().build();
   const document = SwaggerModule.createDocument(app, config);
+  app.useGlobalPipes(new ValidationPipe());
   SwaggerModule.setup('doc', app, document);
   await app.listen(3002);
 }
