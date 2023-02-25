7f5bda5ea5dbb67dbc232d78861e1aaf0d372b79
yoshiki495 <yoshiki-tanaka@cuebic.co.jp>
2023-02-25 18:13:00 +0900

    merge

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { TweetModule } from './tweet/tweet.module';
+import { CorsMiddleware } from './cors.middleware';
 
 @Module({
   imports: [
@@ -23,4 +24,8 @@ import { TweetModule } from './tweet/tweet.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorsMiddleware).forRoutes('*');
+  }
+}
